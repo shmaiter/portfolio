96ef85df2ac811c7745f3ce395f51848ccd814fd
Shmaither <shmaither@gmail.com>
2022-10-30 17:22:26 -0600

    refactor: projects and about pages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const autoType = document.querySelector(".auto-type");
 
 let typed = new Typed(".auto-type", {
-    strings: ["a software developer", "a visual artist?^500 .^500 .^500 .\n`probably only in good days`", "a block-chain enthusiast, sci-fi lover, green house owner."],
+    strings: ["a software developer.", "a visual artist?^500 .^500 .^500 .\n`probably only in good days.`", "a block-chain enthusiast, sci-fi lover, green house owner."],
     typeSpeed: 80,
     backSpeed: 5,
     loop: true,
